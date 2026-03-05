953b0320ce8e23089914997c5cafa3de2600ba44
Zosin0 <lucaszoser1@gmail.com>
2026-03-04 23:27:27 -0300

    chore: deploy + 3 components missing on curriculum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 import { NavComponent } from './components/nav/nav.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
-import { ProjetosComponent } from './components/projetos/projetos.component';
 import { SobreComponent } from './components/sobre/sobre.component';
 import { ModalComponent } from './components/modal/modal.component';
 
@@ -18,7 +17,6 @@ import { ModalComponent } from './components/modal/modal.component';
     HabilidadesComponent,
     NavComponent,
     PerfilComponent,
-    ProjetosComponent,
     SobreComponent,
     ModalComponent
   ],
